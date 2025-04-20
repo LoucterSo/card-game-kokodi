e4d96514229ce8c5cce2ff7fc6b2b998250d8afe
LoucterSo <loucterso@gmail.com>
2025-04-20 14:45:33 +0300

    Getting started

---
+++ src/main/java/io/github/LoucterSo/card_game_kokodi/CardGameKokodi.java
@@ -0,0 +1,13 @@
+package io.github.LoucterSo.card_game_kokodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameKokodi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGameKokodi.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/LoucterSo/card_game_kokodi/CardGameKokodiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.LoucterSo.card_game_kokodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameKokodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
